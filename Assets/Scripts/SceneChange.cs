53a28b9f3e97cb49bd5c8da7bfa0b0e1c85c2f58
rnrgll <shuajjjjj@naver.com>
2023-11-17 15:36:47 +0900

    dohyun Add:Chapter scene background 완성

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangetoHome()
     {
-
+        SceneManager.LoadScene("Home");
+    }
+    public void ChangetoChapter()
+    {
+        SceneManager.LoadScene("Chapter");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
