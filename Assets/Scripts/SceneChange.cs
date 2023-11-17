aa8526d91b75c13f36d3ca4e9257b1fafb6452ba
rnrgll <shuajjjjj@naver.com>
2023-11-17 17:08:42 +0900

    dohyun Add:Ch1Stage graphic 추가

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -5,13 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    //홈으로 씬 전환
     public void ChangetoHome()
     {
         SceneManager.LoadScene("Home");
     }
+
+    //챕터 선택 씬으로 전환
     public void ChangetoChapter()
     {
         SceneManager.LoadScene("Chapter");
     }
+
+
+
+    //챕터별 스테이지 씬으로 전환
+    public void ChangetoCh1Stage()
+    {
+        SceneManager.LoadScene("Ch1Stage");
+    }
+    public void ChangetoCh2Stage()
+    {
+        SceneManager.LoadScene("Ch2Stage");
+    }
+    public void ChangetoCh3Stage()
+    {
+        SceneManager.LoadScene("Ch3Stage");
+    }
+
+
+
+
 
 }
