c5ca0efa5157a92b6921cd6bd0d40d649ee0a981
rnrgll <shuajjjjj@naver.com>
2023-11-19 19:59:13 +0900

    dohyun Feat:setting panel 추가 & setting panel 슬라이드 ui 구현

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class DataManager : MonoBehaviour
+{
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
