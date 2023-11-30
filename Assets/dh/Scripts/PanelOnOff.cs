c34746110250bec8f388af14dbe5e8eddc521489
rnrgll <shuajjjjj@naver.com>
2023-11-30 20:31:03 +0900

    dohyun : setting panel 클릭 이벤트 수정, tmp 오류 수정

--- Assets/dh/Scripts/PanelOnOff.cs
+++ Assets/dh/Scripts/PanelOnOff.cs
@@ -7,8 +7,14 @@ public class PanelOnOff : MonoBehaviour
 {
     public GameObject Panel;
 
+    private void Start()
+    {
+        Panel.SetActive(false);
+    }
+
     public void PanelVisible()
     {
+        Debug.Log("클릭");
         Panel.SetActive(true);
     }
 
--- Assets/dh/Scripts/SetInvisible.cs
+++ Assets/dh/Scripts/SetInvisible.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SetInvisible : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         this.gameObject.SetActive(false);
     }
