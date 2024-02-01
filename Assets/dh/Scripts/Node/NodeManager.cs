702dd8c2526bcbd1986cdb5ac2156e88498a3f5c
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2024-02-02 07:03:31 +0900

    jiwoo : 노드매니저 코루틴 수정

--- Assets/dh/Scripts/Node/NodeManager.cs
+++ Assets/dh/Scripts/Node/NodeManager.cs
@@ -53,13 +53,20 @@ public class NodeManager : MonoBehaviour
     //컴파일
     public void Run()
     {
+       
+
+
         //큐 초기화
         // ClearNodeQueue();
-        //find start node
-        startNode = GameObject.FindGameObjectWithTag("startNode");
-        currentNode = startNode;
+
         try
         {
+            //find start node
+            startNode = GameObject.FindGameObjectWithTag("startNode");
+            currentNode = startNode;
+
+            StartCoroutine(ExcuteNode());
+
             // while (currentNode.GetComponent<NodeNameManager>().NodeName != "EndNode")
             // {
             //     Debug.Log("while문 수행");
@@ -73,21 +80,7 @@ public class NodeManager : MonoBehaviour
 
             // }
 
-            //함수 노드 때문에 EndNode까지 반복문 돌리는 걸로 수정
-            while (currentNode.GetComponent<NodeNameManager>().NodeName != "EndNode")
-            {
-                Debug.Log("실행 시작");
-                //현재 노드를 큐에 추가
-                StartCoroutine(ExcuteNode(currentNode.GetComponent<INode>()));
-                //현재 노드의 Flow outPort 찾기
-                currentFlowoutPort = currentNode.GetComponent<IFollowFlow>().NextFlow();
-                Debug.Log("현재 플로우 아웃포트 " + currentFlowoutPort);
-                //Flow outPort로 다음 node 찾아서 currentNode 업데이트
-                currentNode = NextNode(currentFlowoutPort);
-
-            }
-
-            Debug.Log("Run Complete");
+           
         }
         catch (NullReferenceException e)
         {
@@ -97,21 +90,28 @@ public class NodeManager : MonoBehaviour
     }
 
 
-
-    // public void ExecuteNodes()
-    // {
-    //     StartCoroutine(ExcuteNode());
-    // }
-
-
-    IEnumerator ExcuteNode(INode currentNode)
+    IEnumerator ExcuteNode()
     {
+        //함수 노드 때문에 EndNode까지 반복문 돌리는 걸로 수정
+        while (currentNode.GetComponent<NodeNameManager>().NodeName != "EndNode")
+        {
+            Debug.Log("실행 시작");
+            //현재 노드 실행 후 끝날 때까지 기다리기
+            yield return currentNode.GetComponent<INode>().Execute();
+            //현재 노드의 Flow outPort 찾기
+            currentFlowoutPort = currentNode.GetComponent<IFollowFlow>().NextFlow();
+            Debug.Log("현재 플로우 아웃포트 " + currentFlowoutPort);
+            //Flow outPort로 다음 node 찾아서 currentNode 업데이트
+            currentNode = NextNode(currentFlowoutPort);
+        }
+        Debug.Log("Run Complete");
+        yield return null;
         // while (nodesToExecute.Count != 0)
         // {
         //     //큐에서 노드 꺼내기
         //     INode currentNode = nodesToExecute.Dequeue();
         //     Debug.Log(currentNode);
-        yield return StartCoroutine(currentNode.Execute());
+
         // // }
         // yield return null;
     }
