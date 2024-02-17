c8637908d35dc573c0f6e7065a338927c4f6b63d
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2024-02-17 10:37:06 +0900

    jiwoo : 다이얼로그 시스템 에셋 적용

--- Assets/KJW/Scripts/Scripts_Dialogue/DialogueSystem.cs
+++ Assets/KJW/Scripts/Scripts_Dialogue/DialogueSystem.cs
@@ -13,25 +13,28 @@ public class DialogueSystem : MonoBehaviour
     [SerializeField] private GameObject left_Panel;
     [SerializeField] private Text left_name_text;
     [SerializeField] private Image left_standing_image;
+    [SerializeField] private Text dialogue_text_L;
 
     [Header("Right Panel")]
     [SerializeField] private GameObject right_Panel;
     [SerializeField] private Text right_name_text;
     [SerializeField] private Image right_standing_image;
+    [SerializeField] private Text dialogue_text_R;
 
     [Header("ETC")]
     [SerializeField] private GameObject etc;
     public List<GameObject> dialogues;
-    [SerializeField] private Text dialogue_text;
     [NonSerialized] public EachDialogue[] nowDialogueList;
-    public Button left_buttons;
-    public Button right_buttons;
-    public Button end_button;
+    //public Button left_buttons;
+    //public Button right_buttons;
+    //public Button end_button;
 
     int diaIndex;
     int diaListIndex;
     private string fullText;
     private string currentText = "";
+    private bool isDoneTyping;
+    private bool stopTyping;
     private Text typingText;
     public float typingSpeed = 0.05f;
     public AudioSource audioSource;
@@ -39,11 +42,13 @@ public class DialogueSystem : MonoBehaviour
     void Start()
     {
         diaListIndex = 0;
-        end_button.gameObject.SetActive(false);
+        //end_button.gameObject.SetActive(false);
         audioSource = GetComponent<AudioSource>();
         left_Panel.gameObject.SetActive(false);
         right_Panel.gameObject.SetActive(false);
         etc.SetActive(false);
+        isDoneTyping = false;
+        stopTyping = false;
     }
 
     public void StartSpeak()
@@ -51,39 +56,57 @@ public class DialogueSystem : MonoBehaviour
         diaIndex = 0;
         diaListIndex++;
         dialogues[diaListIndex-1].gameObject.SetActive(false);
-        left_buttons.gameObject.SetActive(false);
+        //left_buttons.gameObject.SetActive(false);
         etc.SetActive(true);
         Speak(nowDialogueList[diaIndex].GetSpeaker(), nowDialogueList[diaIndex].dialogueText, nowDialogueList[diaIndex].GetFace());
     }
 
     public void NextSpeak()
     {
-        end_button.gameObject.SetActive(false);
-        left_buttons.gameObject.SetActive(true);
-        diaIndex++;
-        Speak(nowDialogueList[diaIndex].GetSpeaker(), nowDialogueList[diaIndex].dialogueText, nowDialogueList[diaIndex].GetFace());
-        if (diaIndex >= nowDialogueList.Length - 1) // if It is 0th dialogue, Not active Left arrow button 
+        if(!isDoneTyping) // íƒ€ì´í•‘ ì•ˆëë‚¬ìœ¼ë©´ ë°”ë¡œ ì¶œë ¥
         {
-            right_buttons.gameObject.SetActive(false);
-            end_button.gameObject.SetActive(true);
+            stopTyping = true;
+            currentText = fullText;
+            typingText.text = currentText;
+        }
+        else
+        {
+            if (diaIndex >= nowDialogueList.Length - 1)
+            {
+                EndDialogue();
+            }
+            else
+            {
+                //end_button.gameObject.SetActive(false);
+                // left_buttons.gameObject.SetActive(true);
+                diaIndex++;
+                Speak(nowDialogueList[diaIndex].GetSpeaker(), nowDialogueList[diaIndex].dialogueText, nowDialogueList[diaIndex].GetFace());
+                //if (diaIndex >= nowDialogueList.Length - 1) // if It is 0th dialogue, Not active Left arrow button 
+                //{
+                //    //right_buttons.gameObject.SetActive(false);
+                //    //end_button.gameObject.SetActive(true);
+                //}
+            }
         }
     }
 
 
     public void PrevSpeak()
     {
-        right_buttons.gameObject.SetActive(true);
+        ///right_buttons.gameObject.SetActive(true);
         diaIndex--;
         Speak(nowDialogueList[diaIndex].GetSpeaker(), nowDialogueList[diaIndex].dialogueText, nowDialogueList[diaIndex].GetFace());
-        if(diaIndex == 0) // if It is 0th dialogue, Not active Left arrow button 
-        {
-            left_buttons.gameObject.SetActive(false);
-        }
+        //if(diaIndex == 0) // if It is 0th dialogue, Not active Left arrow button 
+        //{
+        //    left_buttons.gameObject.SetActive(false);
+        //}
     }
 
 
     void Speak(Speaker speaker, string dia, Dialogue.Face face)
     {
+        isDoneTyping = false;
+        stopTyping = false;
         currentText = ""; // Empty text
         fullText = dia;
         if (speaker.isPlayer) 
@@ -96,7 +119,7 @@ public class DialogueSystem : MonoBehaviour
         right_Panel.SetActive(false);
         left_Panel.SetActive(true);
         left_name_text.text = speaker.speaker_name;
-        typingText = dialogue_text;
+        typingText = dialogue_text_L;
         StartCoroutine(ShowText());
         left_standing_image.sprite = speaker.standing_sprites[(int)face];
         left_standing_image.SetNativeSize();
@@ -107,7 +130,7 @@ public class DialogueSystem : MonoBehaviour
         left_Panel.SetActive(false);
         right_Panel.SetActive(true);
         right_name_text.text = speaker.speaker_name;
-        typingText = dialogue_text;
+        typingText = dialogue_text_R;
         StartCoroutine(ShowText());
         right_standing_image.sprite = speaker.standing_sprites[(int)face];
         right_standing_image.SetNativeSize();
@@ -124,10 +147,10 @@ public class DialogueSystem : MonoBehaviour
     IEnumerator ShowText()
     {
         int index = diaIndex;
-
-        for (int i = 0; i <= fullText.Length; i++)
+        
+        for (int i = 0; i <= fullText.Length && !stopTyping; i++)
         {
-            // ÄÚ·çÆ¾ÀÌ ½ÃÀÛµÉ ¶§ÀÇ diaIndex °ª°ú ÇöÀç diaIndex °ªÀÌ ´Ù¸£¸é ÁßÁö
+            // ï¿½Ú·ï¿½Æ¾ï¿½ï¿½ ï¿½ï¿½ï¿½Ûµï¿½ ï¿½ï¿½ï¿½ï¿½ diaIndex ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ diaIndex ï¿½ï¿½ï¿½ï¿½ ï¿½Ù¸ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
             if (index != diaIndex)
             {
                 yield break;
@@ -140,6 +163,8 @@ public class DialogueSystem : MonoBehaviour
                 audioSource.PlayOneShot(audioSource.clip);
             }
             yield return new WaitForSeconds(typingSpeed);
+            if(currentText == fullText) isDoneTyping = true;
         }
+        yield return null;
     }
 }
