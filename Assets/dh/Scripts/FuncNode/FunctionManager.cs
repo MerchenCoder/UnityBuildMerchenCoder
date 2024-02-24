c7fe0764ab99bc0135a811d2084697498a074011
rnrgll <shuajjjjj@naver.com>
2024-02-24 15:50:21 +0900

    dohyun : Delete Mode 스위치간 동기화 구현

--- Assets/dh/Scripts/FuncNode/FunctionManager.cs
+++ Assets/dh/Scripts/FuncNode/FunctionManager.cs
@@ -195,8 +195,8 @@ public class FunctionManager : MonoBehaviour
         functionInstance = Instantiate(functionPrefabs[funcInsType]);
         int[] paraTypes = new int[] { para1Type, para2Type };
         string[] paraNames = new string[] { para1Name, para2Name };
-        Debug.Log(para2Name);
-        Debug.Log(paraNames);
+        //Debug.Log(para2Name);
+        //Debug.Log(paraNames);
         //functionInstance port type & function name 설정
         functionInstance = SetFuncNode(functionInstance, type, funcName, paraTypes, paraNames, returnType);
 
