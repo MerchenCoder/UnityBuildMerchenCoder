bdf2f2cf74cb0ff018358ad5c3cc064d98829361
rnrgll <shuajjjjj@naver.com>
2024-03-01 12:01:02 +0900

    Update: Quest3, Quest4 (AB 선택형 퀴즈) 한 문제당 시도횟수 1번으로 수정

--- Assets/dh/Scripts/NodeMenu/RefreshLayout.cs
+++ Assets/dh/Scripts/NodeMenu/RefreshLayout.cs
@@ -5,20 +5,16 @@ using UnityEngine.UI;
 
 public class RefreshLayout : MonoBehaviour
 {
-    public GameObject layout1;
-    public GameObject layout2;
 
-    private void OnEnable()
+    public void RebuildLayout()
     {
-        Debug.Log("활성화");
-        Refresh();
+        StartCoroutine(nameof(CorRebuildLayout), (RectTransform)transform);
     }
-    public void Refresh()
+
+    IEnumerator CorRebuildLayout(RectTransform obj)
     {
-        Debug.Log("refresh");
-        // LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)layout2.transform); //content sizefitter 버그 해결을 위한 리프레시
-        // LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)layout1.transform); //content sizefitter 버그 해결을 위한 리프레시
-        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)this.transform); //content sizefitter 버그 해결을 위한 리프레시
+        yield return new WaitForEndOfFrame();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(obj);
 
     }
 }
