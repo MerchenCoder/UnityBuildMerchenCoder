0f200d0b693a02975e06f786e232ba605db3bb4e
rnrgll <shuajjjjj@naver.com>
2024-04-09 12:44:13 +0900

    dohyun : 스크립트 수정 및 디버그용 버튼 추가

--- Assets/KJW/Scripts/Scripts_Home/ItemControl.cs
+++ Assets/KJW/Scripts/Scripts_Home/ItemControl.cs
@@ -22,7 +22,15 @@ public class ItemControl : MonoBehaviour
         setButton.gameObject.SetActive(false);
 
         transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = thisItem.item_Image;
-        transform.GetChild(0).GetChild(0).GetComponent<Image>().color = _thisItem.GetComponent<SpriteRenderer>().color;
+        if (_thisItem.GetComponent<SpriteRenderer>() != null)
+        {
+            transform.GetChild(0).GetChild(0).GetComponent<Image>().color = _thisItem.GetComponent<SpriteRenderer>().color;
+        }
+        else
+        {
+            transform.GetChild(0).GetChild(0).GetComponent<Image>().color = _thisItem.transform.GetComponentInChildren<SpriteRenderer>(true).color;
+        }
+
         // transform.GetChild(0).GetChild(0).GetComponent<Image>().SetNativeSize();
         _itemNameTxt.text = thisItem.item_name;
         _itemPriceTxt.text = thisItem.price;
