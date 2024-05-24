28c560ca9ae13e2f6462a019cc8929c1b11646ee
rnrgll <shuajjjjj@naver.com>
2024-05-24 16:43:25 +0900

    dohyun : 조건 수정

--- Assets/MJ/Scripts/Ch02Control/MorganActive.cs
+++ Assets/MJ/Scripts/Ch02Control/MorganActive.cs
@@ -7,19 +7,7 @@ public class MorganActive : MonoBehaviour
     // Start is called before the first frame update
     void Update()
     {
-        if (GameManager.Instance.CheckPlayProgress("Mission2-10N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-10Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-11N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-11Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-12N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-12Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-13N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-13Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-14N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-14Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-15N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-15Y")
-            || GameManager.Instance.CheckPlayProgress("Chap2Ending"))
+        if (GameManager.Instance.CheckPlayProgress("Mission2-9Y"))
         {
             gameObject.SetActive(true);
         }
--- Assets/MJ/Scripts/Ch02Control/WallSetActive3.cs
+++ Assets/MJ/Scripts/Ch02Control/WallSetActive3.cs
@@ -7,18 +7,7 @@ public class WallSetActive3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GameManager.Instance.CheckPlayProgress("Mission2-10Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-11N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-11Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-12N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-12Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-13N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-13Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-14N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-14Y")
-            || GameManager.Instance.CheckPlayProgress("Mission2-15N")
-            || GameManager.Instance.CheckPlayProgress("Mission2-15Y")
-            || GameManager.Instance.CheckPlayProgress("Chap2Ending"))
+        if (GameManager.Instance.CheckPlayProgress("Mission2-10N"))
         {
             gameObject.SetActive(false);
         }
