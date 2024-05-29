3ca5ae8290cbafe54425c67754034c3793fda84e
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-05-30 01:02:55 +0900

    MJ_ch02_WatcherSleep
    감시자 문제 깨면 잠자게 만듬

---
+++ Assets/MJ/Scripts/Ch02Control/WatcherSleep.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WatcherSleep : MonoBehaviour
+{
+    // 자식 객체들을 관리하기 위한 리스트
+    private List<Transform> childObjects = new List<Transform>();
+
+    // 이미지가 들어 있는 객체에 대한 참조
+    public GameObject imageObject;
+
+    public RectTransform imageRectTransform;
+
+    void Start()
+    {
+        // 자식 객체들을 리스트에 추가
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject != imageObject)
+            {
+                childObjects.Add(child);
+            }
+        }
+        if (imageObject != null)
+        {
+            imageRectTransform = imageObject.GetComponent<RectTransform>();
+            Debug.Log("Make ImageRectTransform Success");
+        }
+        else
+            Debug.Log("Make IRT Fail");
+    }
+
+    void Update()
+    {
+        if (DataManager.Instance.gameStateData.ch2MissionClear[13])
+        {
+            Debug.Log("경비병이 쓰러져있는 조건을 충족함");
+            FlipImageY(true);
+            RotateImage();
+            //gameObject.transform.localPosition = new Vector3(12.4144f, -3.564708f, 0);
+        }
+    }
+
+    bool rotated = false; // Add this line as a class member
+
+    void RotateImage()
+    {
+        Debug.Log("This is Function RotateImage");
+
+        if (imageObject != null && !rotated)
+        {
+            // Rotate the main object
+            imageObject.transform.localEulerAngles = new Vector3(0, 0, 270);
+            Debug.Log("Rotated imageObject to: " + imageObject.transform.localEulerAngles);
+
+            // Find the CharCanvas child object
+            Transform charCanvas = transform.Find("CharCanvas");
+
+            if (charCanvas != null)
+            {
+                // Find the quiz_btn child within CharCanvas
+                Transform quizBtn = charCanvas.Find("quiz_Btn");
+                Transform dialBtn = charCanvas.Find("dialogue_Btn_After_2_14");
+
+                if (quizBtn != null)
+                {
+                    // Log current position
+                    Debug.Log("Current position of quiz_Btn: " + quizBtn.localPosition);
+
+                    // Adjust position of the quiz_Btn
+                    float newPosX = (float)-10.3;
+                    float newPosY = (float)33.4;
+                    quizBtn.localPosition = new Vector3(newPosX, newPosY, quizBtn.localPosition.z);
+                    dialBtn.localPosition = new Vector3(newPosX, newPosY, dialBtn.localPosition.z);
+
+                    // Log new position
+                    Debug.Log("New position of quiz_Btn: " + quizBtn.localPosition);
+
+                    // Adjust rotation of the quiz_Btn
+                    quizBtn.localEulerAngles = new Vector3(180, 0, -270);
+                    dialBtn.localEulerAngles = new Vector3(180, 0, -270);
+                    Debug.Log("Adjusted rotation for quiz_Btn to: " + quizBtn.localEulerAngles);
+
+                    // Set the flag to true to prevent further adjustments
+                    rotated = true;
+                }
+                else
+                {
+                    Debug.LogError("quiz_btn not found under CharCanvas.");
+                }
+            }
+            else
+            {
+                Debug.LogError("CharCanvas not found.");
+            }
+        }
+        else if (imageObject == null)
+        {
+            Debug.LogError("imageObject is null");
+        }
+        else if (rotated)
+        {
+            Debug.LogWarning("Rotation already adjusted, skipping further adjustments.");
+        }
+    }
+
+
+
+    void ResetImageRotation()
+    {
+        // AnnaSleepObject의 회전을 초기화
+        if (imageObject != null)
+        {
+            transform.localEulerAngles = Vector3.zero;
+
+            // 하위 객체들의 회전도 초기화
+            foreach (Transform child in childObjects)
+            {
+                child.localEulerAngles = Vector3.zero;
+            }
+        }
+    }
+
+    void FlipImageY(bool flip)
+    {
+        Debug.Log("This is Function FlipImageY");
+        float scale = imageRectTransform.eulerAngles.y;
+        scale = flip ? 180 : 0;
+        Debug.Log("SCale is " + scale);
+        // Get the current rotation and modify the y-axis rotation
+        Vector3 currentRotation = imageRectTransform.eulerAngles;
+        currentRotation.y = scale;
+
+        // Apply the new rotation
+        imageRectTransform.eulerAngles = currentRotation;
+    }
+}
